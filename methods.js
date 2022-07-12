544ccde195fcf3e33f721ab5d57d252431c6a38d
SHAHYASHNILESH <91772554+SHAHYASHNILESH@users.noreply.github.com>
2022-07-12 12:38:20 +0530

    Update methods.js

--- methods.js
+++ methods.js
@@ -5,8 +5,25 @@ const app=express();
 app.use(express.json());
 app.listen(3000);
 
-let users={}
+let users=[
+    {
+        'id':1,
+        'Name':'Yash'
+    },
+
+    {
+        'id':2,
+        'Name':'Nilesh'
+    },
+    {
+        'id':3,
+        'Name':'Pinal'
+    }
+
+];
+
 app.get('/users',(req,res)=>{
+    console.log(req.query);
     res.send(users);
 })
 
@@ -16,5 +33,32 @@ app.post('/users',(req,res)=>{
     res.json({
         message:"Data received successfully",
         user:req.body
+    });
+});
+
+app.patch('/users',(req,res)=>{
+    console.log(req.body);
+    let data=req.body;
+    for(key in req.body){
+        //console.log(key);
+        users[key]=data[key];
+        //console.log(users);
+    }
+    res.json({
+        message:"Data updated successfully"
     })
+});
+
+app.delete('/users',(req,res)=>{
+    users={};
+    res.json({
+        message:"Data has been successfully deleted"
+    });
+});
+
+
+app.get('/users/:username',(req,res)=>{
+    console.log(req.params.username);
+    console.log(req.params);
+    res.send("Data received successfully");
 })
