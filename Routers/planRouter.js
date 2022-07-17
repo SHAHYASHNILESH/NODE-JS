9a5333fe8363dd71ce9b3eafe03b5a61574cb999
SHAHYASHNILESH <91772554+SHAHYASHNILESH@users.noreply.github.com>
2022-07-17 22:20:39 +0530

    Create planRouter.js

---
+++ Routers/planRouter.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const { isAuthorised, protectRoute } = require('../controller/authController');
+const planRouter=express.Router();
+
+//gets all plans
+planRouter
+.route('/getAllPlans')
+.get(getAllPlans)
+
+//own plan->check logged in or not 
+planRouter.use(protectRoute)
+planRouter
+.route('/plan/:id')
+.get(getPlan)
+
+//admin and restaurant owner can create,update and delete operations 
+planRouter.use(isAuthorised['admin','owner'])
+planRouter
+.route('/crudplan')
+.post(createPlan)
+.patch(updatePlan)
+.delete(deletePlan)
